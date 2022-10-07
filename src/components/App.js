d379877345c20b34cc4d833ecba83a03b0424477
Omkar patil <omkarp666@gmail.com>
2022-10-07 23:27:03 +0530

    React Fizz Buzz

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import '../styles/App.css';
+
+
 const App = () => {
-const[count,setCount]=useState(1)
-
-    const IncrementHandler=()=>{
-          setCount(count+1)
-    }
-
-    const DecrementHandler=()=>{
-        if(count != 1){
-            setCount(count-1)
-        }
-
-    }
   
+  const [count, setCount] = useState(1);
+  
+
+  const IncNum =()=>{
+    // console.log('clicked')
+    setCount(count + 1);
+  }
+
+  const DecNum =()=>{
+    // console.log('clicked')
+    if(count != 1){
+    setCount(count - 1);
+    }
+  }
+  
+  
+
   return (
     <div id="main">
-     <div id='counter'>
-        <h2 className= {count % 5 == 0 && count % 3 == 0 ? 'fizzbuzz' : (count % 3 == 0) ? 'fizz' : (count % 5 == 0 ) ? 'buzz' : 'normal'}>{count}</h2>
-        <button id='increment' onClick={IncrementHandler}>Increments</button>
-        <button id='decrement' onClick={DecrementHandler}>Decrements</button>
-    </div>
 
+    <button id="increment" onClick={IncNum}>Increments</button>  
+    
+    <div id='counter' className= {(count % 5 == 0 && count % 3 == 0) ? 'fizzbuzz' : (count % 3 == 0) ? 'fizz' : (count % 5 == 0 ) ? 'buzz' : 'normal'}>{count}</div>
+    
+    <button id="decrement" onClick={DecNum}>decrements</button>
     </div>
   )
+  
 }
 
 
