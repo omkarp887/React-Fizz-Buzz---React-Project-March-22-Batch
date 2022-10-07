823e9fbff78e718cc043843628207d89b2125180
Omkar patil <omkarp666@gmail.com>
2022-10-07 20:37:28 +0530

    React-Fizz-Buzz---React-Project-March-22-Batch

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 import '../styles/App.css';
-const App = () => {
 
+
+const App = () => {
+  const state = useState();
+  const [count, setCount] = useState(0);
   
+
+  const IncNum =()=>{
+    // console.log('clicked')
+    setCount(count + 1);
+  }
+
+  const DecNum =()=>{
+    // console.log('clicked')
+    if(count > 0){
+    setCount(count - 1);
+    }
+  }
+  
+  
+
   return (
     <div id="main">
+    <button onClick={IncNum}>Increment</button>
 
+    <h1 className= {count % 5 == 0 && count % 3 == 0 ? 'fizzbuzz' : (count % 3 == 0) ? 'fizz' : (count % 5 == 0 ) ? 'buzz' : 'normal'}>{count}</h1>
+    
+    <button onClick={DecNum}>decrement</button>
     </div>
   )
+  
 }
 
 
